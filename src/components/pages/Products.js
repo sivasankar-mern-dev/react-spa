1c9e1ec0958f8d5ee10214f6d3b7c647dd340792
Siva Sankar R <52097841+spookysiva@users.noreply.github.com>
2020-10-01 16:55:29 +0530

    initial commit

---
+++ src/components/pages/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+
+export default function Products() {
+    return (
+        <>
+        <h1 className="products">Products</h1>
+        <Footer />
+        </>
+    );
+}
